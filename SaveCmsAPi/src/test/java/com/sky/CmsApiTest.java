2e7c5687b1b8b21f86bf2002b7cc9fbde0d15383
raawan <minalfulzele@gmail.com>
2015-01-22 18:56:42 +0000

    added second failing test: triangulation mechanism.

--- SaveCmsAPi/src/test/java/com/sky/CmsApiTest.java
+++ SaveCmsAPi/src/test/java/com/sky/CmsApiTest.java
@@ -1,9 +1,12 @@
 package com.sky;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Date;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.junit.Test;
 
 
@@ -19,4 +22,20 @@ public class CmsApiTest {
 		assertTrue(new CmsApi().checkTileExistForGivenDateAndPosition(tile,schedule));
 	}
 	
+	@Test
+	public void GIVEN_inputTilePositionAndTimeDoNotCollidesWithSomeOtherExstingTilesInSchedule_THEN_returnFalse() {
+		
+		ClientSchedule schedule = new ClientSchedule();
+		Tile tile = new Tile("tileLabel",new Position(2,3), new Date());
+		schedule.getTiles().add(tile);
+		Date scheduledDateOfNewTile = createDate(2015, 1, 20, 5, 33, DateTimeZone.getDefault());
+		Tile tileToAdd = new Tile("tileLabel",new Position(2,3), scheduledDateOfNewTile);
+		assertFalse(new CmsApi().checkTileExistForGivenDateAndPosition(tileToAdd,schedule));
+	}
+	
+	private Date createDate(int year, int monthOfYear, int dayOfMonth, int hourOfDay, int minuteOfHour, DateTimeZone zone) {
+		
+		 DateTime dt = new DateTime(year,monthOfYear,dayOfMonth,hourOfDay,minuteOfHour,zone);
+		 return dt.toDate();
+	}
 }
