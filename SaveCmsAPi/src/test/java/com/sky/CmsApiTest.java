f4c41e4a48051b33418ab41d3a863ba53a3e3400
raawan <minalfulzele@gmail.com>
2015-01-22 17:40:51 +0000

    Programming by intention - thought first test case - with compilation error

--- SaveCmsAPi/src/test/java/com/sky/CmsApiTest.java
+++ SaveCmsAPi/src/test/java/com/sky/CmsApiTest.java
@@ -1,8 +1,22 @@
 package com.sky;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+
+import org.junit.Test;
+
 
 
 public class CmsApiTest {
 	
+	@Test
+	public void GIVEN_inputTilePositionAndTimeCollidesWithSomeOtherExstingTileInSchedule_THEN_returnTrue() {
+		
+		Schedule schedule = new Schedule();
+		Tile tile = new Tile("tileLabel",new Position(2,3), new Date());
+		schedule.getTiles().add(tile);
+		assertTrue(new CmsApi().checkTileExistForGivenDateAndPosition(tile,schedule));
+	}
 	
 }
