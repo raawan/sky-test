e6b33c72e7347a9b3207092f337138537423bcb6
raawan <minalfulzele@gmail.com>
2015-01-23 22:21:04 +0000

    test 2

--- SaveCmsAPi/src/test/java/com/sky/ClientScheduleTest.java
+++ SaveCmsAPi/src/test/java/com/sky/ClientScheduleTest.java
@@ -3,6 +3,7 @@ package com.sky;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Date;
+import java.util.Iterator;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
@@ -28,6 +29,44 @@ public class ClientScheduleTest {
 		
 		assertEquals(tileSchedule2,clientSchedule.getClientTileSchedules().first());
 		assertEquals(tileSchedule1,clientSchedule.getClientTileSchedules().last());
+	}
+	
+	@Test
+	public void GIVEN_aListOfClientSchedule_THEN_ClientScheduleShouldBeAddedInChronologicalOrder_2() {
+		
+		ClientSchedule clientSchedule = new ClientSchedule();
+		
+		Tile tile1 = createTile(createLabel("label1"), createPosition(1, 1));
+		Date startDate1 = createDate(2015,1,2,13,40,getDefaultTimeZone());
+		TileSchedule tileSchedule1 = createTileSchedule(tile1, startDate1);
+		
+		Tile tile2 = createTile(createLabel("label2"), createPosition(1, 1));
+		Date startDate2 = createDate(2013,2,2,13,30,getDefaultTimeZone());
+		TileSchedule tileSchedule2 = createTileSchedule(tile2, startDate2);
+		
+		Tile tile3 = createTile(createLabel("label3"), createPosition(1, 1));
+		Date startDate3 = createDate(2015,1,2,14,44,getDefaultTimeZone());
+		TileSchedule tileSchedule3 = createTileSchedule(tile3, startDate3);
+		
+		Tile tile4 = createTile(createLabel("label4"), createPosition(1, 1));
+		Date startDate4 = createDate(2015,1,2,13,31,getDefaultTimeZone());
+		TileSchedule tileSchedule4 = createTileSchedule(tile4, startDate4);
+		
+		addTileToSchedule(clientSchedule,tileSchedule1);
+		addTileToSchedule(clientSchedule,tileSchedule2);
+		addTileToSchedule(clientSchedule,tileSchedule3);
+		addTileToSchedule(clientSchedule,tileSchedule4);
+		
+		assertEquals(tileSchedule2,clientSchedule.getClientTileSchedules().first());
+		assertEquals(tileSchedule3,clientSchedule.getClientTileSchedules().last());
+		assertEquals(tileSchedule1,clientSchedule.getClientTileSchedules().higher(tileSchedule4));
+		
+		Iterator<TileSchedule> itr = clientSchedule.getClientTileSchedules().iterator();
+		TileSchedule tmp = null;
+		while(itr.hasNext()) {
+			tmp = itr.next();
+			System.out.println(tmp.getTile().getLabel().getLabel() + ":" +tmp.getStartDate());
+		}
 	}
 	
 	private DateTimeZone getDefaultTimeZone() {
