3983aff1c0fa8f196e8b4d6c7bb914603f86a2f1
raawan <minalfulzele@gmail.com>
2015-01-25 15:02:27 +0000

    added failed test case
    tile validation

---
+++ SaveCmsAPi/src/test/java/com/sky/TileTest.java
@@ -0,0 +1,13 @@
+package com.sky;
+
+import org.junit.Test;
+
+public class TileTest {
+
+	
+	@Test(expected=InvalidTileException.class)
+	public void GIVEN_BlankLabel_THEN_InvalidTileException() throws InvalidTileException {
+		
+		Tile tile = new Tile(new Label(""),new Position(2,3));
+	}
+}
